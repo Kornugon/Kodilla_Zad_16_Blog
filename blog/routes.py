5eef28c05c76e1d5841d71092b3de33de3005020
Piotr Loba <piotr.loba22@gmail.com>
2021-01-30 22:27:24 +0100

    Commit for refactoring task 16.2

--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
-from blog import app, db
+from flask import render_template, request, flash, url_for, redirect
+from blog import app
 from blog.models import Entry, db
+from blog.forms import EntryForm
 
 from faker import Faker
 
@@ -16,7 +17,15 @@ def generate_entries(how_many=10):
         )
         db.session.add(post)
     db.session.commit()
+    all_posts = Entry.query.all()
+    return all_posts
 
+
+def delete_posts():
+    all_posts = Entry.query.all()
+    for post in all_posts:
+        db.session.delete(post)
+    db.session.commit()
 
 
 @app.route("/")
@@ -24,3 +33,52 @@ def index():
     all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
     #generate_entries(how_many=10)
     return render_template("homepage.html", all_posts=all_posts)
+
+
+@app.route("/gen/")
+def generate_posts():
+    delete_posts()
+    all_posts = generate_entries(how_many=10)
+    return render_template("homepage.html", all_posts=all_posts)
+
+
+def get_entries(entry_id):
+    errors = None
+    if entry_id != 0:
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+        flash_mess = f'Edytowano post {entry.title} na blogu.'
+
+    else:
+        form = EntryForm()
+        entry = Entry(
+            title=form.title.data,
+            body=form.body.data,
+            is_published=form.is_published.data
+            )
+        flash_mess = f'Dodano post {entry.title} na bloga.'
+        
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if entry_id != 0:
+                form.populate_obj(entry)
+            else:
+                db.session.add(entry)
+            db.session.commit()
+            flash(flash_mess)
+            return redirect(url_for("index"))
+        else:
+            errors = form.errors
+
+    return render_template("entry_form.html", form=form, errors=errors)
+
+
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+    return get_entries(entry_id=0)
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id):
+    return get_entries(entry_id)
