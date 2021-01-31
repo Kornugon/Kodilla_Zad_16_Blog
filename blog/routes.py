1ba5c8083f08b8bca6fe59e74b81dac1e40a9c17
Piotr Loba <piotr.loba22@gmail.com>
2021-01-31 16:24:33 +0100

    Add login_required to generate posts and add utils.py

--- blog/routes.py
+++ blog/routes.py
@@ -1,64 +1,11 @@
 from flask import render_template, request, flash, url_for, redirect, session
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm, LoginForm
+from blog.forms import LoginForm
+from blog.utils import generate_fake_posts, delete_posts, get_entries
+
 import functools
 
-from faker import Faker
-
-
-def generate_entries(how_many=10):
-    fake = Faker()
-
-    for _ in range(how_many):
-        post = Entry(
-            title=fake.sentence(),
-            body='\n'.join(fake.paragraphs(15)),
-            is_published=True
-        )
-        db.session.add(post)
-    db.session.commit()
-    all_posts = Entry.query.all()
-    return all_posts
-
-
-def delete_posts():
-    all_posts = Entry.query.all()
-    for post in all_posts:
-        db.session.delete(post)
-    db.session.commit()
-
-
-def get_entries(entry_id):
-    errors = None
-    if entry_id != 0:
-        entry = Entry.query.filter_by(id=entry_id).first_or_404()
-        form = EntryForm(obj=entry)
-        flash_mess = f'Edytowano post {entry.title} na blogu.'
-
-    else:
-        form = EntryForm()
-        entry = Entry(
-            title=form.title.data,
-            body=form.body.data,
-            is_published=form.is_published.data
-            )
-        flash_mess = f'Dodano post {entry.title} na bloga.'
-        
-
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            if entry_id != 0:
-                form.populate_obj(entry)
-            else:
-                db.session.add(entry)
-            db.session.commit()
-            flash(flash_mess)
-            return redirect(url_for("index"))
-        else:
-            errors = form.errors
-
-    return render_template("entry_form.html", form=form, errors=errors)
 
 
 
@@ -69,50 +16,6 @@ def login_required(view_func):
            return view_func(*args, **kwargs)
        return redirect(url_for('login', next=request.path))
    return check_permissions
-
-
-@app.route("/")
-def index():
-    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
-    return render_template("homepage.html", all_posts=all_posts)
-
-
-@app.route("/drafts/", methods=['GET'])
-@login_required
-def list_drafts():
-    drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
-    return render_template("drafts.html", drafts=drafts)
-
-
-@app.route("/delete_post/<int:entry_id>", methods=['POST'])
-@login_required
-def delete_entry(entry_id):
-    if request.method == 'POST':
-        entry = Entry.query.filter_by(id=entry_id).first_or_404()
-        db.session.delete(entry)
-        db.session.commit()
-        flash(f'Usunięto post {entry.title} na blogu.')
-    return redirect(url_for('index'))
-
-
-@app.route("/gen/")
-def generate_posts():
-    delete_posts()
-    all_posts = generate_entries(how_many=10)
-    flash(f'Usunięto stare posty i wygenerowano 10 nowych randomowych.')
-    return render_template("homepage.html", all_posts=all_posts)
-
-
-@app.route("/new-post/", methods=["GET", "POST"])
-@login_required
-def create_entry():
-    return get_entries(entry_id=0)
-
-
-@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
-@login_required
-def edit_entry(entry_id):
-    return get_entries(entry_id)
 
 
 @app.route("/login/", methods=['GET', 'POST'])
@@ -137,3 +40,50 @@ def logout():
        session.clear()
        flash('You are now logged out.', 'success')
    return redirect(url_for('index'))
+
+
+
+
+@app.route("/")
+def index():
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+    return render_template("homepage.html", all_posts=all_posts)
+
+
+@app.route("/drafts/", methods=['GET'])
+@login_required
+def list_drafts():
+    drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+    return render_template("drafts.html", drafts=drafts)
+
+
+@app.route("/gen/")
+@login_required
+def generate_posts():
+    delete_posts()
+    all_posts = generate_fake_posts(how_many=10)
+    flash(f'Usunięto stare posty i wygenerowano 10 nowych randomowych.')
+    return render_template("homepage.html", all_posts=all_posts)
+
+
+@app.route("/delete_post/<int:entry_id>", methods=['POST'])
+@login_required
+def delete_entry(entry_id):
+    if request.method == 'POST':
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        db.session.delete(entry)
+        db.session.commit()
+        flash(f'Usunięto post {entry.title} na blogu.')
+    return redirect(url_for('index'))
+
+
+@app.route("/new-post/", methods=["GET", "POST"])
+@login_required
+def create_entry():
+    return get_entries(entry_id=0)
+
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
+def edit_entry(entry_id):
+    return get_entries(entry_id)
